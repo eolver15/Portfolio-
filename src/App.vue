9b03e8beb5cf682e25f5eefb42c158c470557bf6
eolver15 <eolver15@stu.southtexascollege.edu>
2019-10-15 11:23:35 -0500

    update image for superawesomeapp

--- src/App.vue
+++ src/App.vue
@@ -55,13 +55,13 @@ export default {
         {
         name: 'Marvel Finder',
         description:'Using Marvel Public API, we have a simple character finder application, with character descriptions and some comics where the characters appear.',
-        screenshot:'/images/marvel finder.JPG',
+        screenshot:'/images/Marvel Finder.JPG',
         url:'https://marvelfinder.eolver15.site',
       },
        {
         name: 'Super Awesome App',
         description:'The coolest and better Social Network ever created (by me).',
-        screenshot:'/images/super-awesome.JPG',
+        screenshot:'/images/superawesomeapp.jpg',
         url: 'https://superawesomeapp.eolver15.site',
       },
       {
